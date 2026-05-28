0b818ea42327e9acfc7dbba6b34f31163ca9503b
MatheusBastosBarbosa <bastosbarbosa9@gmail.com>
2026-05-28 17:26:39 -0300

    initial commit

---
+++ src/main/java/com/equipe4/escolaservice/EscolaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.equipe4.escolaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaServiceApplication.class, args);
+	}
+
+}
