b48d80e045d644af58ffd9bbf2922fdbb6a9e35b
MatheusBastosBarbosa <bastosbarbosa9@gmail.com>
2026-05-31 20:56:02 -0300

    feat: Controller com endpoints REST da Escola

---
+++ src/main/java/com/equipe4/escolaservice/controller/EscolaController.java
@@ -0,0 +1,38 @@
+package com.equipe4.escolaservice.controller;
+
+import com.equipe4.escolaservice.dto.CriarEscolaDto;
+import com.equipe4.escolaservice.dto.EscolaResponseDto;
+import com.equipe4.escolaservice.service.EscolaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/escola")
+public class EscolaController {
+
+    private final EscolaService escolaService;
+
+    public EscolaController(EscolaService escolaService) {
+        this.escolaService = escolaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UUID> criarEscola(@RequestBody CriarEscolaDto dto) {
+        var id = escolaService.criarEscola(dto);
+        return ResponseEntity.ok(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EscolaResponseDto>> findAllEscolas() {
+        return ResponseEntity.ok(escolaService.findAllEscolas());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirEscola(@PathVariable UUID id) {
+        escolaService.excluirEscola(id);
+        return ResponseEntity.noContent().build();
+    }
+}
