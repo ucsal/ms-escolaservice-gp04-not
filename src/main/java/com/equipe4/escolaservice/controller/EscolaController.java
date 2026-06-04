f852a46fed080617e26e3866e063bf35006ff609
MatheusBastosBarbosa <bastosbarbosa9@gmail.com>
2026-06-04 09:49:19 -0300

    del: crocs

--- src/main/java/com/equipe4/escolaservice/controller/EscolaController.java
+++ src/main/java/com/equipe4/escolaservice/controller/EscolaController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/escola")
 public class EscolaController {
